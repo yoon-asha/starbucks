4cc5df7b385b0d6a014a63b7938617b63940ed3b
yoon-asha <asha_@kakako.com>
2021-05-11 18:53:24 +0900

    Start project

--- js/youtube.js
+++ js/youtube.js
@@ -1,26 +1,23 @@
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
+// 2. This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
 
- tag.src = "https://www.youtube.com/iframe_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
- function onYouTubeIframeAPIReady() {
-  //<div id="player"></div>
-  new YT.Player('player', { //아이디라고 #안붙여도됨. 그냥 이렇게 쓰면 player 라는 아이디를 찾음
-    videoId: 'An6LvWQuj_8', //최초 재생할 유튜브 영상 ID
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
     playerVars: {
-      autoplay: true, //자동 재생 유무
+      autoplay: true, // 자동 재생 유무
       loop: true, // 반복 재생 유무
-      playlist:'An6LvWQuj_8' //반복 재생할 유튜브 영상 ID 목록
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
       onReady: function (event) {
-       event.target.mute() //음소거
+        event.target.mute() // 음소거
       }
     }
   });
- }
-
+}
